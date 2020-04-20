3e3265c533dd99baea9f85effd231e374ab26076
LidaHak <61308358+LidaHak@users.noreply.github.com>
2020-04-20 13:23:20 +0400

    Update food calories.py

--- food calories.py
+++ food calories.py
@@ -1,107 +1,89 @@
 import json
-import csv
+import datetime
+import os
+from usda import UsdaClient
 
-print("Hello, this is the app to calculate your calories. PLease input the folowing informaton. ")
-while(True):
-    try:
-      weight = int(input("Enter your weight(in kg): "))
-      break
-    except:
-         print("The answer is invalid")
-while(True):
-    try:
-      height = int(input("Enter your height(in cm): "))
-      break
-    except:
-         print("The answer is invalid")
-while(True):
-    try:
-      age = int(input("Enter your age(in years): "))
-      break
-    except:
-         print("The answer is invalid")
-BMR1 = 0
-while(True):
-    gender = input("Male or Female?")
-    if gender.lower() == 'male':
-        BMR1 += (10 * weight) + (6.25 * height) - (5 * age) + 5
-        print(BMR1)
-        break
-    else:
-        if gender.lower() == 'female':
-            BMR1 += (10 * weight) + (6.25 * height) - (5 * age) - 161
-            print(BMR1)
+
+def read_user_data():
+    while True:
+        try:
+            weight = int(input("Enter your weight(in kg): "))
+            height = int(input("Enter your height(in cm): "))
+            age = int(input("Enter your age(in years): "))
+            break
+        except ValueError:
+            print("Please, enter correct values!")
+    while True:
+        gender = input("Male or Female? ")
+        if gender.lower() == 'male':
+            BMR1 = (10 * weight) + (6.25 * height) - (5 * age) + 5
+            # print(BMR1)
+            break
+        elif gender.lower() == 'female':
+            BMR1 = (10 * weight) + (6.25 * height) - (5 * age) - 161
+            # print(BMR1)
             break
         else:
-            print("The answer is invalid")
-while(True):
-    try:
-        activitystatus = input('''Please choose one of the options
-                            1.sedentary (little or no exercise)
-                            2.lightly active (light exercise/sports 1-3 days/week)
-                            3.moderately active (moderate exercise/sports 3-5 days/week)
-                            4.very active (hard exercise/sports 6-7 days a week)
-                            5.extra active (very hard exercise/sports & physical job or 2x training)
-                            ''')
-        if activitystatus == "1":
+            print("Please, enter correct values!")
+    while True:
+        activity_status = input('''Please choose one of the options \r\tEnter '1' if sedentary (little or no 
+        exercise) \r\tEnter '2' if lightly active (light exercise/sports 1-3 days/week) \r\tEnter '3' if moderately 
+        active (moderate exercise/sports 3-5 days/week) \r\tEnter '4' if active (hard exercise/sports 6-7 days a 
+        week) \r\tEnter '5' if extra active (very hard exercise/sports & physical job or 2x training) \r\t> ''')
+        if activity_status == "1":
             BMR2 = BMR1 * 1.2
-            print(BMR2)
             break
-        if activitystatus == "2":
+        elif activity_status == "2":
             BMR2 = BMR1 * 1.375
-            print(BMR2)
             break
-        if activitystatus == '3':
+        elif activity_status == '3':
             BMR2 = BMR1 * 1.55
-            print(BMR2)
             break
-        if activitystatus == '4':
+        elif activity_status == '4':
             BMR2 = BMR1 * 1.725
-            print(BMR2)
             break
-        if activitystatus == '5':
+        elif activity_status == '5':
             BMR2 = BMR1 * 1.9
-            print(BMR2)
             break
         else:
-            print("The answer is invalid")
-    except:
-        pass
-while(True):
-    try:
-        goals = int(input('''Please, choose one of the following options: I want to
-                        1. loose weight,
-                        2. mantain my weight,
-                        3. gain weight
-                        4. get to an ideal weight
-                        '''))
-        break
-    except Exception:
-        print("The answer is invalid")
-if goals == '1':
-    BMR2 -= 500
-if goals == '2':
-    BMR2 = BMR2
-if goals == '3':
-    amount = input("How much you want to gain during the week? 1 pound(~0,4kg) or 2 pounds(~0,9kg)?")
-    if goals == "1":
-        BMR2 += 500
-    else:
-        BMR2 += 1000
-print("So, this is the amount of calories you shall take to reach your goal:  " + str(BMR2))
-# userinitialdata
+            print("Please, enter correct values!")
+    # print(BMR2)
+    while True:
+        goals = input('''Please, choose one of the following options: I want to
+                         \rEnter '1' if you want to loose weight,
+                         \rEnter '2' if you want to maintain my weight,
+                         \rEnter '3' if you want to gain weight
+                         \r> ''')
+        if goals == '1':
 
-userinitialdata = {'weight': weight,
-                    'height': height,
-                    'age': age,
-                    'gender': gender
-                    }
-with open("data_file.json", "w") as write_file:
-    json.dump(userinitialdata, write_file)
-
-with open("data_file.json", "r") as read_file:
-    data = json.load(read_file)
-
+            amount = input("How much you want to loose during the week? 1 pound(~0,4kg) or 2 pounds(~0,9kg)? ")
+            if amount == '1':
+                BMR2 -= 500
+            else:
+                BMR2 -= 1000
+            break
+        if goals == '2':
+            BMR2 = BMR1
+            break
+        if goals == '3':
+            amount = input("How much you want to gain during the week? 1 pound(~0,4kg) or 2 pounds(~0,9kg)?")
+            if amount == "1":
+                BMR2 += 500
+            else:
+                BMR2 += 1000
+            break
+        else:
+            print("Please, enter correct values!")
+    print("So, this is the amount of calories you shall take to reach your goal: " + str(BMR2))
+    user_initial_info = {
+        'weight': weight,
+        'height': height,
+        'age': age,
+        'gender': gender,
+        'calories_limit': BMR2}
+    with open("data_file.json", "w+") as writeFile:
+        json.dump(user_initial_info, writeFile, indent=True)
+    return BMR2
 
 
 
